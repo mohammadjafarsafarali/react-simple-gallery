8d965e9e77ae2f2d0413c44e52337e9ac28420b9
RaselHaulader <hauladerrasel2@gmail.com>
2022-03-13 01:37:11 +0600

    fix some responsive bug

--- src/components/HomeGallery/GalleryItem.js
+++ src/components/HomeGallery/GalleryItem.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Gallery.css';
 
-const GalleryItem = ({img,idx,place}) => {
+const GalleryItem = ({ img, idx, place }) => {
     const bgStyle = {
         backgroundImage: `url(${img})`,
     }
     return (
-        <div  className={ `galleryItemBody ${idx === 1 && ' bigItem'}`} >
-          <div style={bgStyle} className='galleryImg'>
-          </div>
-          <div className='galleryImageOverlay'><h3>{place}</h3></div>
+        <div className={`galleryItemBody ${idx === 1 && ' bigItem'}`} >
+            <div style={bgStyle} className='galleryImg'>
+            </div>
+            <div className='galleryImageOverlay'><h3>{place}</h3></div>
         </div>
     );
 };
